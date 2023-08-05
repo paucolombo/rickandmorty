917df0b952d8233a27e2d36a5acbc113912159fb
paucolombo <mpaucolombo@gmail.com>
2023-08-05 18:04:02 +0200

    imgs url changes

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Buttons = () => {
+
+  // const showCharacters = async () => {
+  //   const data = await fetch('https://rickandmortyapi.com/api/character').then((res) => res.json());
+  //   return data
+  // }
+
+  return (
+    <div className='buttonsContainer'>
+      <button>Show Characters</button>
+      {/* <ul>
+        <li><label>
+          <input type="checkbox" name="Alien" id="Alien" value="Alien"></input>
+          Alien</label></li>
+        <li><label>
+          <input type="checkbox" name="Human" id="Human" value="Human"></input>
+          Human</label></li>
+      </ul>
+      <button>Filter</button> */}
+    </div >
+  )
+
+}
+
+export default Buttons
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div className='logo'>
-      <img src="../public/logo.png" alt="Rick and Morty" />
+      <img src="/logo.png" alt="Rick and Morty" />
     </div>
   )
 }
