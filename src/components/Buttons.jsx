707f63dd9f58524dc6dd0f8617aa0c86be41d230
paucolombo <mpaucolombo@gmail.com>
2023-09-15 17:18:30 +0200

    improve performance adding react lazy

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import Characterlist from './Characterlist';
+import React, { lazy, Suspense } from 'react'; // Importa Suspense aquí
+const Characterlist = lazy(() => import('./Characterlist'));
 
 const Buttons = () => {
   const [visible, setVisible] = React.useState(true);
 
   return (
     <>
-      {visible && <div className='buttonsContainer'><button
-        onClick={() => {
-          setVisible(false);
-        }}
-      >
-        Show all Characters
-      </button></div>}
-      {!visible && <Characterlist />}
-
+      {visible && (
+        <div className='buttonsContainer'>
+          <button
+            onClick={() => {
+              setVisible(false);
+            }}>
+            Show all Characters
+          </button>
+        </div>
+      )}
+      {!visible && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Characterlist />
+        </Suspense>
+      )}
     </>
-  )
+  );
+};
 
-}
-export default Buttons
+export default Buttons;
